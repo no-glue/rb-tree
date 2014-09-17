875c8f834ba69b757aaf7b14948618a5c0d0e552
no-glue <nikola_pav@eunet.rs>
2014-09-17 14:36:41 +0200

    simple insert

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ using namespace std;
 
 int main() {
   RedBlackTree<RedBlackNode<string, vector<string> >, string> * tree = new RedBlackTree<RedBlackNode<string, vector<string> >, string>();
+  RedBlackNode<string, vector<string> > * found;
   tree->insert("a", "a");
   tree->insert("b", "b");
   tree->insert("c", "c");
   tree->insert("d", "d");
   tree->insert("e", "e");
+  found = tree->find("e");
+  cout<<"found "<<found->value[0]<<endl;
   delete tree;
   return 0;
 }
