85c739d7d976255e055e49490eeed1390459cbbc
no-glue <nikola_pav@eunet.rs>
2014-09-27 20:26:12 +0200

    tree insert

--- red_black_tree.h
+++ red_black_tree.h
@@ -15,22 +15,41 @@ public:
   }
 private:
   Node * root;
-  void insert(Str key, Str value) {
+  void insert(Str key, Str value, Node * & root) {
     // insert key and value
     Node * inserted_node = new Node(key, value);
-    bool inserted = tree_insert(inserted_node); // todo insert_tree
-    if(inserted) insert_fixup_for(inserted_node);
+    bool inserted = tree_insert(inserted_node, root);
+    if(inserted) insert_fixup_for(inserted_node, root); // todo insert_fixup_for
   }
-  bool tree_insert(Node * inserted_node) {
+  bool tree_insert(Node * inserted_node, Node * & root) {
     // scan the tree, insert node
-    Node * parent = tree_find_last(inserted_node->key); // todo tree_find_last
-    return tree_insert_child(parent, inserted_node); // todo tree_insert_child
+    Node * parent = tree_find_last(inserted_node->key, root);
+    return tree_insert_child(parent, inserted_node, root);
   }
-  Node * tree_find_last(Str key) {
+  Node * tree_find_last(Str key, Node * & root) {
     // find place to insert new node
+    Node * walk = root, * previous = NULL;
+    while(walk) {
+      previous = walk;
+      if(key < walk->key && (walk = walk->left)) {}
+      else if(key > walk->key && (walk = walk->right)) {}
+      else return walk;
+    }
+    return previous;
   }
-  bool tree_insert_child(Node * parent, Node * inserted_node) {
+  bool tree_insert_child(Node * parent, Node * inserted_node, Node * & root) {
     // add child to parent
+    if(!parent) root = inserted_node;
+    else {
+      if(inserted_node->key < parent->key) parent->left = inserted_node;
+      else if(inserted_node->key > parent->key) parent->right = inserted_node;
+      else return false;
+    }
+    inserted_node->parent = parent;
+    return true;
+  }
+  void insert_fixup_for(Node * inserted_node, Node * & root) {
+    // fixup for inserted node
   }
   Node * find(Str key, Node * & root) {
     // find a node given a key
